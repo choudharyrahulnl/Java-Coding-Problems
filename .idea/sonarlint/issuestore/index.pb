026dd7007ede569d2f3091e59107a0c0bee8d0e9
Choudhary Rahul, SUB-SDC-NL-DOS-TDE <Rahul.Choudhary@swisscom.com>
2023-01-19 08:44:38 +0100

    commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+j
+:src/main/java/com/coding/problem/java/JavaApplication.java,7/6/761f6c836400af24d829654b617e6a2d3aee3d57
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+x
+Hsrc/main/java/com/coding/problem/java/CountDuplicateCharacters/Main.java,9/3/9365a3394a9c6f76e4b359940d3a67d9a75a7198
+ƒ
+Ssrc/main/java/com/coding/problem/java/CountDuplicateCharacters/CountDuplicates.java,2/3/23c57272266437c056b8b0208427f8d319902164
---
+++ src/main/java/com/coding/problem/java/CountDuplicateCharacters/CountDuplicates.java
@@ -0,0 +1,27 @@
+package com.coding.problem.java.CountDuplicateCharacters;
+
+import java.util.Map;
+
+public class CountDuplicates {
+
+    public void sum(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static Map<Character, Long> countDuplicateCharactersV1(String text){
+        return null;
+    }
+
+    public static Map<Character, Long> countDuplicateCharactersV2(String text) {
+        return null;
+    }
+
+    public static Map<String, Integer> countDuplicateCharactersVCP1(String textCp) {
+        return null;
+    }
+
+    public static Map<String, Long> countDuplicateCharactersVCP2(String textCp) {
+        return null;
+    }
+}
